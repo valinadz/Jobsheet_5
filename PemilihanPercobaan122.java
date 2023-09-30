c93e9d7b29887abfb09426f5fec1a7c2a79c800b
valinadz <valinaadzra@gmail.com>
2023-09-30 22:44:33 +0700

    modifikasi

--- PemilihanPercobaan122.java
+++ PemilihanPercobaan122.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class PemilihanPercobaan122 {
     public static void main (String [] args) {
 
+        String hasil;
+
         Scanner input22 = new Scanner(System.in);
 
         System.out.println("Masukkan angka:  ");
         int angka = input22.nextInt();
 
-        if (angka % 2 == 0)
-            System.out.println("Angka "+angka+" bilangan genap");
-        else
-            System.out.println("Angka "+angka+" bilangan ganjil");
+        hasil = (angka % 2 == 0) ? "Angka "+angka+" bilangan genap" : "Angka "+angka+" bilangan ganjil";
+        System.out.println(hasil);
 
     }
 }
